48692f6c424352baea4375d59d144d7cb61c2f20
Neumann-Peter <mail.neumann.peter@gmail.com>
2023-03-22 09:34:04 +0100

    initial commit

---
+++ kocsma/src/main/java/hu/kocsma/wysio/kocsma/KocsmaApplication.java
@@ -0,0 +1,13 @@
+package hu.kocsma.wysio.kocsma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KocsmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KocsmaApplication.class, args);
+	}
+
+}
