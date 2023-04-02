4548387a5102b1f8c4616b00c8d7ff3c6e34ed48
Neumann-Peter <mail.neumann.peter@gmail.com>
2023-04-02 15:37:35 +0200

    adding VendegRepository and ResourceNotFoundException

---
+++ src/main/java/hu/kocsma/wysio/kocsma/repository/VendegRepository.java
@@ -0,0 +1,8 @@
+package hu.kocsma.wysio.kocsma.repository;
+
+import hu.kocsma.wysio.kocsma.model.Vendeg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendegRepository extends JpaRepository<Vendeg, Long> {
+
+}
